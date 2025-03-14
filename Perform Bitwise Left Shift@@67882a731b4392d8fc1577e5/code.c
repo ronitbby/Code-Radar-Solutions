59cc30123d19c14758484a4c1601ce786f6aaaa1
ronitbby <ronit0699.becse24@chitkara.edu.in>
2025-03-15 00:00:45 +0530

    Solution to Perform Bitwise Left Shift

---
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input two integers
+    scanf("%d %d", &a, &b);
+
+    // Perform and print the bitwise OR operation
+    printf("b<<1 = %u\n", b << 1)
+    return 0;
+}
